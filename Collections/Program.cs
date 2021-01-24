fac06f1524f8c437c7266d93b5fa124911a35d9e
Nisa Nur Baydaş <n.nurb@hotmail.com>
2021-01-24 11:53:50 +0300

    .

---
+++ Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Ya da daha esnek bir yapı olan list sınıfını kullanırsın
+            List<string> names2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+
+            names2.Add("İlker"); 
+            //bunu yaptığın zaman var olan adreste bir eleman daha eklemiş olursun
+            //diğer değerleri kaybetmeden
+            Console.WriteLine(names2  [4]);
+            Console.WriteLine(names2[0]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        // sen bana Product türünde bir şey göndereceksin ben de onu product ismiyle tutup(adresini) kullanacam
+        //diyor bu method
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.")
+        }
+
+    }
+}
